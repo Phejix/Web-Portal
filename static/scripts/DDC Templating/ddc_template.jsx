1b52ab7d08d95c89798b8340babaebeff8be6d99
Josh Chandler <33601498+Phejix@users.noreply.github.com>
2019-02-25 16:02:45 +0000

    Delete ddc_template.jsx

--- static/scripts/DDC Templating/ddc_template.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import DDCFrontpage from "./DDCFrontpage.jsx"
-
-ReactDOM.render(<DDCFrontpage />, document.getElementById("ddc-template-root"));
